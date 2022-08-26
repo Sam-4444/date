e917541b8fae8f9f00c54efd4b8e180b73dcf127
Sam <106349608+Sam-4444@users.noreply.github.com>
2022-08-26 19:21:39 +0430

    Update birthday.py

--- birthday.py
+++ birthday.py
@@ -1,12 +1,19 @@
 import datetime
-
-
 import calendar
+
+
+
+# printing the calendar
 y=int(input("input the year :"))
 x=int(input("input the month :"))
 print(calendar.month(y,x))
 
-
+# printing the year of birth
 age=int(input("enter your age (in years):"))
 now=datetime.datetime.now().year
 print(f"birthday(year) :{now-age}")
+
+
+# age by seconds
+age=input("enter your age and press enter ! ")
+print("your age  is (by seconds ): "+str(int((3.156)*(10**7)*int(age))))
